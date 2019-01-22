3b148eda521d4d145859aa20c285b1bc7bfaa8bf
wajeehhasan <wajeeh.hasan322@gmail.com>
2019-01-22 23:33:17 +0500

    listings schema created

--- firstapp/settings.py
+++ firstapp/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,8 +78,11 @@ WSGI_APPLICATION = 'firstapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'nopass',
+        'HOST': 'localhost'
     }
 }
 
